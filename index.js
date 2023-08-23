77bf21aabd2c5310f629391799ad7d5283fb479c
Ona <onushkis@gmail.com>
2023-08-23 18:07:57 +0200

    added knap assigment

---
+++ index.js
@@ -0,0 +1,5 @@
+const minKnap = document.querySelector("#knapElement");
+ minKnap.addEventListener("click", funktionDerKaldesVedKlick);
+ function funktionDerKaldesVedKlick() {
+    console.log("du er klikket på knappen");
+ }
