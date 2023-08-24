db2a287802409b44cfe647d90dfb6db169c43011
Ona <onushkis@gmail.com>
2023-08-24 15:42:09 +0200

    added changes nav bar + responsive

---
+++ book.js
@@ -0,0 +1,10 @@
+const firstBook = {
+    title:"dienos sviesa",
+    autor:"Tokien",
+    genre: ["Fantasy", "Drama"],
+    pages: 751,
+    hasRead:true,
+    hasIliustrations:false
+}
+
+console.log();firstBook.autor
