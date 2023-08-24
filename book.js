7e78ecf21a8e0315ba8e2cb9244a760bd888e52b
Ona <onushkis@gmail.com>
2023-08-24 22:01:04 +0200

    added changes

--- book.js
+++ book.js
@@ -7,4 +7,17 @@ const firstBook = {
     hasIliustrations:false
 }
 
-console.log();firstBook.autor
+console.log();firstBook.autor;
+
+const person ={
+    "fornavn": "Ona",
+    "efternavn":"Girdzijauskaite",
+    "email":"onushkis@gmail.com",
+    "billede": "./assets/ona.jpg"
+}
+
+const destination = document.querySelector("#person");
+const fuldNavn = `${person.fornavn} ${person.efternavn}`;
+destination.querySelector("h3").textContent = fuldNavn;
+destination.querySelector("p").textContent = person.email;
+destination.querySelector("img").src = person.billede;
