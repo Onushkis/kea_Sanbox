6f7b5e8c7a5274ea227d23b051ce10052855cb19
Ona <onushkis@gmail.com>
2023-08-28 00:31:10 +0200

    sorter

--- Beers Alan Version/beersAlan.js
+++ Beers Alan Version/beersAlan.js
@@ -3,7 +3,8 @@ fetch("https://api.punkapi.com/v2/beers")
   .then(vis);
 
 const temp = document.querySelector("template").content;
-const parent = document.querySelector("section");
+const light = document.querySelector(".light");
+const dark = document.querySelector(".dark");
 
 function vis(data) {
   data.forEach((beer) => {
@@ -17,12 +18,26 @@ function vis(data) {
       // add a class if there is twist
       klon.querySelector("details span").textContent = beer.method.twist;
     }
+
+    // Alcohol
+    if(beer.abv > 5.5){
+        klon.querySelector("article").classList.add("strong");
+    }
     klon.querySelector("p span").textContent = beer.abv;
     klon.querySelector("p+p span").textContent = beer.ebc;
 
+    // hvis den var over støtre 17 så det var øl mørk
+    if(beer.ebc > 17){
+        dark.appendChild(klon);
+    }else{
+        light.appendChild(klon);
+    }
+
+
     // klon at the end!!!
-    parent.appendChild(klon);
+    dark.appendChild(klon);
   });
 }
 
-// Hvis der er en twist så hvis dem
+// genere en element med css
+
